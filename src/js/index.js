7b9697a8707142ddce51c55999d47e4608cb556b
Rodrigo Jimenez <jimenezrodry12gmail.com>
2022-03-15 18:22:33 -0500

    Hice la section de nuestros pastores

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,5 @@
-$(function(){
-    $(window).scroll(function() {
+$(function () {
+    $(window).scroll(function () {
         var header = $(".nav-main");
         header.toggleClass("scroll", window.scrollY > 0);
     });
