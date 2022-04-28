a863ad24cc0b9271728d47f7b2cf2ad4c58471b6
Rodrigo Jimenez <jimenezrodry12gmail.com>
2022-04-28 18:20:40 -0500

    Imagenes y el navbar

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,61 @@
 $(function () {
+
+    const glider = new Glider(document.querySelector('.carousel-lista'), {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        dots: ".carousel-indicador",
+        scrollLockDelay: 250,
+        arrows: {
+            prev: ".carousel-anterior",
+            next: ".carousel-siguiente",
+        },
+        // responsive: [
+        //     {
+        //         // screens greater than >= 775px
+        //         breakpoint: 800,
+        //         settings: {
+        //             // Set to `auto` and provide item width to adjust to viewport
+        //             slidesToShow: 1,
+        //             slidesToScroll: 1
+        //         }
+        //     }, {
+        //         // screens greater than >= 1024px
+        //         breakpoint: 1024,
+        //         settings: {
+        //             slidesToShow: 1,
+        //             slidesToScroll: 1
+        //         }
+        //     }
+        // ]
+    });
+
+    function sliderAuto(slider, miliseconds) {
+        const slidesCount = slider.track.childElementCount;
+        let slideTimeout = null;
+        let nextIndex = 1;
+
+        function slide() {
+            slideTimeout = setTimeout(function () {
+                if (nextIndex >= slidesCount) {
+                    nextIndex = 0;
+                }
+                slider.scrollItem(nextIndex++);
+            }, miliseconds);
+        }
+
+        slider.ele.addEventListener("glider-animated", function () {
+            window.clearInterval(slideTimeout);
+            slide();
+        });
+
+        slide();
+    }
+
+    sliderAuto(glider, 3000);
+
+
     $(window).scroll(function () {
         var header = $(".nav-main");
-        header.toggleClass("scroll", window.scrollY > 0);
+        header.toggleClass("scroll", window.scrollY > 80);
     });
 });
